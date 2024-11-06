f896d0216c767ec647efde12e7262ac846f738e4
Mistry <2309639@cognizant.com>
2024-11-06 11:31:13 +0530

    add footer

--- barber-shop/script.js
+++ barber-shop/script.js
@@ -8,7 +8,6 @@ function nav(){
 }
 
 function navHighlite(){
-    var navLinks = document.getElementsByClassName('nav-link');
     var sections = document.querySelectorAll('section');
 
     var observer = new IntersectionObserver(entries => {
@@ -26,7 +25,7 @@ function navHighlite(){
             }
         });
     },{
-        threshold: 0.5 // 50% of the section needs to be visible
+        threshold: 0.8 // 80% of the section needs to be visible
     });
 
     //This part loop through all the selected section on the page and call observer
